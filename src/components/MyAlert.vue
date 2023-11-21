82aa93b30f1c2f82e7c566e084799595510125a2
june0 <june0@acornsoft.io>
2023-11-21 17:02:31 +0900

    Alert 기능, 환경 변수 설정.

---
+++ src/components/MyAlert.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { storeToRefs } from "pinia";
+
+import { useAlertStore } from "@/stores"; // @ : src 를 의미
+
+const alertStore = useAlertStore();
+const { alert } = storeToRefs(alertStore);
+</script>
+
+<template>
+  <div v-if="alert" class="container">
+    <div class="m-3">
+      <!-- :class javascript 의 동적인 변수구나, v-bind 가 생략된 것 -->
+      <div class="alert alert-dismissable" :class="alert.type">
+        <button @click="alertStore.clear()" class="btn btn-link close">
+          &times;
+        </button>
+        {{ alert.message }}
+      </div>
+    </div>
+  </div>
+</template>
